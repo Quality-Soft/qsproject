635c18498517233926ea585e9aac90739d12b335
emmerson <eofs@cin.ufpe.br>
2015-10-26 23:35:57 +0000

    não existe mais o botão HOME na barra horizontal

--- projeto-01/config/routes.rb
+++ projeto-01/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :home
   resources :meis
   resources :layouts
-  root 'meis#index'
+  root 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
