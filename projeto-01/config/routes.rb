49df515a9aa2d3809726fdcebc27be9ec2e93aa2
emmerson <eofs@cin.ufpe.br>
2015-10-27 00:03:53 +0000

    alterações realizadas com sucesso

--- projeto-01/config/routes.rb
+++ projeto-01/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :meis
   resources :layouts
   root 'home#index'
+  get 'home/index' => 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
