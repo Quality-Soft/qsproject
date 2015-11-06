12516d5646ea49f0ed280d491289d8928b245918
Micael Souza <mss9@cin.ufpe.br>
2015-11-06 01:49:02 +0000

    instalado gem do cucumber

--- projeto-01/spec/controllers/meis_controller_spec.rb
+++ projeto-01/spec/controllers/meis_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe MeisController, type: :controller do
     describe MeisController do
         describe 'searching TMDb' do
             it 'should call the model method that performs TMDb search' do
+            #Meis.should_receive(:find_in_tmdb).with('dsadsa')
             post :search_tmdb, {:search_terms => 'teste'}
             end
         end
